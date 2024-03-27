9a4c0f11739cbbc118add32a88bc04708eaa06b6
萌幻妖姬 <fay1126@gmail.com>
2024-03-27 20:56:29 +0800

    添加vitepressai的后端接口

--- src/main/java/me/betacat/ai/controller/ChatController.java
+++ src/main/java/me/betacat/ai/controller/ChatController.java
@@ -11,9 +11,7 @@ import org.springframework.ai.ollama.OllamaChatClient;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 import java.util.List;
@@ -55,7 +53,7 @@ public class ChatController {
 
         String content = "";
         int i = 0;
-        for(Document document : similarDocuments) {
+        for (Document document : similarDocuments) {
             content += document.getContent() + "";
             System.out.println((++i) + "，相似度：" + document.getMetadata().get("vector_score") + ", 内容: \n" + document.getContent());
         }
@@ -79,5 +77,56 @@ public class ChatController {
         return Map.of("generation", response);
     }
 
+
+    @PostMapping("/ai/askStream")
+    @CrossOrigin
+    public Flux<ChatResponse> generateVitePressDoc(@RequestBody String question) {
+
+        question = "湖人最后的比分是多少？";//question.trim();
+
+
+        System.out.println("问题: " + question);
+
+        List<Document> similarDocuments = vectorStore.similaritySearch(SearchRequest.query(question));
+        System.out.println("\n相似度查询内容数量: " + similarDocuments.size());
+
+        String content = "";
+        int i = 0;
+        for (Document document : similarDocuments) {
+            // TODO 需要限制上下文的大小 MAX_CONTEXT_TOKEN
+            content += document.getContent() + "";
+            System.out.println((++i) + "，相似度：" + document.getMetadata().get("vector_score") + ", 内容: \n" + document.getContent());
+        }
+
+
+        // Ask gpt
+        String userPrompt = """
+                You are a very kindly assistant who loves to help people. Given the following sections from documatation, answer the question using only that information, outputted in markdown format. If you are unsure and the answer is not explicitly written in the documentation, say "Sorry, I don't know how to help with that." Always trying to anwser in the spoken language of the questioner.
+
+                Context sections:
+                ${contextSections}
+
+                Question:
+                ${question}
+
+                Answer as markdown (including related code snippets if available):
+                """;
+
+        Prompt prompt = new Prompt(new UserMessage(userPrompt));
+
+
+//        Message userMessage = new UserMessage(question);
+//
+//        String systemText = """
+//                      请利用如下上下文的信息回答问题，上下文信息如下：{documents}\n
+//                      如果上下文信息中没有帮助,只需说"我不知道"。
+//                """;
+//        SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate(systemText);
+//        Message systemMessage = systemPromptTemplate.createMessage(Map.of("documents", content));
+//
+//        Prompt prompt = new Prompt(List.of(userMessage, systemMessage));
+
+        return chatClient.stream(prompt);
+    }
 
 }
--- src/main/java/me/betacat/ai/controller/EmbeddingController.java
+++ src/main/java/me/betacat/ai/controller/EmbeddingController.java
@@ -1,16 +1,16 @@
 package me.betacat.ai.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.embedding.EmbeddingClient;
 import org.springframework.ai.embedding.EmbeddingResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
 @RestController
+@Slf4j
 public class EmbeddingController {
 
     private final EmbeddingClient embeddingClient;
@@ -25,4 +25,121 @@ public class EmbeddingController {
         EmbeddingResponse embeddingResponse = this.embeddingClient.embedForResponse(List.of(message));
         return Map.of("embedding", embeddingResponse);
     }
+
+
+
+
+    @PostMapping("/ai/upload")
+    public Map upload(@RequestBody Map params) {
+
+        String operation = params.get("operation");
+        const { operation, project = 'default' } = params;
+
+        if (!["add", "delete", "clean", "generate"].includes(operation)) {
+            log.info("Operation {} is not supported. Abort.", operation);
+            context.status(400);
+            return Map.of("error", "Operation {} is not supported. Abort.");
+        }
+        if (operation === 'clean') {
+            // Delete all the stored pages
+            await PagesTable.where({ project }).delete();
+            return Map.of("ok", 1);
+        }
+        if (operation === 'generate') {
+            await generateEmbeddings(project);
+            return Map.of("ok", 1);
+        }
+
+        const { path, title = '', content = '' } = params;
+        if (!path) {
+            console.log('Missing param `path`. Abort.');
+            context.status(400);
+            return Map.of("error", "You are missing param `path`. Abort.");
+        }
+
+        if (operation === 'delete') {
+            // Delete single page
+            await PagesTable.where({ project, path }).delete();
+            return Map.of("ok", 1);
+        }
+
+
+
+
+        // Generate checksum for the page, so we can determine if this page is changed
+          const checksum = crypto.createHash('md5').update(content).digest('hex');
+          const existed = await PagesTable.where({
+                        project,
+                        path,
+                }).findOne();
+
+        if (existed) {
+            if (existed.checksum === checksum) {
+                console.log('This page\'s content is still fresh. Skip regenerating.');
+                return Map.of("ok", 1);
+            } else {
+                // Delete the exist one since we will regenerate it
+                await PagesTable.where({ project, path }).delete();
+            }
+        }
+
+          const chunks = getContentChunks(content);
+          const pagesToSave = chunks.map((chunk, index) => ({
+                        project,
+                        path,
+                        title,
+                        checksum,
+                        chunkIndex: index,
+                        content: chunk,
+                        embedding: null,
+          }))
+
+        // Save the result to database
+        for (let i = 0; i < pagesToSave.length; i += 100) {
+            await PagesTable.save(pagesToSave.slice(i, i + 100));
+        }
+
+
+        return Map.of("ok", 1);
+    }
+
+    private static final int MAX_TOKEN_PER_CHUNK = 8191;
+
+    // Split the page content into chunks base on the MAX_TOKEN_PER_CHUNK
+    private String getContentChunks(String content) {
+          const encoded = tokenizer.encode(content);
+          const tokenChunks = encoded.reduce(
+                        (acc, token) => (
+                        acc[acc.length - 1].length < MAX_TOKEN_PER_CHUNK
+                                ? acc[acc.length - 1].push(token)
+                                : acc.push([token]),
+                        acc
+            ),
+            [[]],
+          );
+        return tokenChunks.map(tokens => tokenizer.decode(tokens));
+    }
+
+
+
+    static final String param = """
+     {"operation":"add",
+     "path":"README.md",
+     "content":"# Documate Vitepress Starter\n\nAn example shows how to integrate Documate into Vitepress site. You can also use this as a template to start a new
+ project.\n"}
+""";
+
+
+    private static final String context = """
+            {"responseHeader":{},"cookies":{},"responseCookies":[],"code":400,"method":"POST","path":"/upload","trigger":"HTTP","rawBody":{"type":"Buffer","data":[123,34,111,112,101,114,97,116,105,111,110,34
+            ,58,34,97,100,100,34,44,34,112,97,116,104,34,58,34,82,69,65,68,77,69,46,109,100,34,44,34,99,111,110,116,101,110,116,34,58,34,35,32,68,111,99,117,109,97,116,101,32,86,105,116,101,112,114,101,115,115,32,83,11
+            6,97,114,116,101,114,92,110,92,110,65,110,32,101,120,97,109,112,108,101,32,115,104,111,119,115,32,104,111,119,32,116,111,32,105,110,116,101,103,114,97,116,101,32,68,111,99,117,109,97,116,101,32,105,110,116,
+            111,32,86,105,116,101,112,114,101,115,115,32,115,105,116,101,46,32,89,111,117,32,99,97,110,32,97,108,115,111,32,117,115,101,32,116,104,105,115,32,97,115,32,97,32,116,101,109,112,108,97,116,101,32,116,111,32
+            ,115,116,97,114,116,32,97,32,110,101,119,32,112,114,111,106,101,99,116,46,92,110,34,125]},"host":"f3v34sknbs.us.aircode.run","url":"/upload","protocol":"https","query":{},"headers":{"host":"production-chfky
+            p-dcdwwryubx.us-west-1-vpc.fcapp.run","user-agent":"axios/1.6.8","content-length":"208","accept":"application/json, text/plain, */*","accept-encoding":"gzip, compress, deflate, br","content-type":"applicati
+            on/json","x-aircode-request-id":"2024032709390735022f831f074","x-forwarded-for":"116.232.52.122, 172.30.159.252","x-forwarded-host":"f3v34sknbs.us.aircode.run","x-forwarded-path":"/upload","x-forwarded-port
+            ":"443","x-forwarded-proto":"https, https","x-real-ip":"116.232.52.122"}}
+                        
+            """;
+
 }
